b2bcaf2400021e9e858f92a1ea40c7fd13d54cb4
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-29 17:15:45 +0000

    Adding while loop to enable to game to repeat

--- main.py
+++ main.py
@@ -6,19 +6,28 @@ screen.title("U.S. States Game")
 screen.addshape("blank_states_img.gif")
 turtle.shape("blank_states_img.gif")
 
-
 data = pandas.read_csv("50_states.csv")
-print(data)
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-file_state = data[data["state"] == answer_state]
-print(file_state["x"], " ", file_state["y"])
+all_states = data.state.to_list()
+print(all_states)
 
-turtle.goto(int(file_state["x"]), int(file_state["y"]))
-turtle.write(f"{file_state['state']}")
-# def get_mouse_click_coor(x, y):
+guessed_states = []
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 Guess the State", prompt=
+    "What's another state's name?").title()
+
+    if answer_state == 'Exit':
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        file_state = data[data["state"] == answer_state]
+        t.goto(int(file_state["x"].item()), int(file_state["y"].item()))
+        t.write(f"{file_state['state'].item()}")
+    # def get_mouse_click_coor(x, y):
 #     print(x, y)
 #
 # turtle.onscreenclick(get_mouse_click_coor)
 
 turtle.mainloop()
-
